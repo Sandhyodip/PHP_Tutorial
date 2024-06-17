ad3f83edadf9f97ca7a4f4e7eaf7ff65277725ff
sandhyodip <sandhyodip@samarth.ac.in>
2024-06-18 01:30:31 +0530

    PHP Dynamic Data

--- practice.php
+++ practice.php
@@ -1,4 +1,21 @@
-<?php
-echo 'Hello Sandhyodip..!! How are you?';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tutorial</title>
+</head>
+<body>
+    <?php 
+    $name='Sandhyodip Das';
+    $designation='Software Developer';
+    ?>
+    <h1>
+        <?php 
+        echo "$name is a $designation";
+        ?>
+    </h1>
 
-?>
+    
+</body>
+</html>
