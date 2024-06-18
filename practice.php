a47d0798378a70a5c3521a027d503687ed81415b
sandhyodip <sandhyodip@samarth.ac.in>
2024-06-18 23:01:25 +0530

    Variables in PHP

--- practice.php
+++ practice.php
@@ -7,15 +7,21 @@
 </head>
 <body>
     <?php 
+    // Single line comment
+    /** 
+     * Multiline comments
+    */
     $name='Sandhyodip Das';
     $designation='Software Developer';
+    $address="<p style='font-weight:600; color:red;'>50, Hazra Para Lane, Bally, Howrah.</p>"
     ?>
     <h1>
         <?php 
         echo "$name is a $designation";
         ?>
     </h1>
-
-    
+    <?php 
+    echo $address;
+    ?>
 </body>
 </html>
